03ee8442a27bc37f8f792a033233d4fc707bcd15
gilbert-ku <gilbert45ku@gmail.com>
2023-08-29 14:49:43 +0300

    Crud

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,7 @@
+INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO cats (name, age, breed) VALUEs ("Lil' Bud", 5 , "American Shorthair");
+INSERT INTO cats (name, age, breed) VALUEs ("Hanna", 1, "Tabby");
+
+-- SELECT id, name, age, breed FROM cats
+-- UPDATE cats SET name = "Hana" WHERE name = "Hannah";  --update
+-- DELETE FROM cats WHERE id = 3;
